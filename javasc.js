1dbb0f3db7efd51ff43c6bb30fdb980e7f765ae7
erickproplays <168188542+erickcationeoss@users.noreply.github.com>
2025-02-21 09:35:02 -0300

    teste1

--- javasc.js
+++ javasc.js
@@ -1,1 +1,19 @@
+//exibe uma mensagem no console
+console.log("Olá mundo!");
 
+//declara uma variável e exibe seu valor
+let nome = "usuário";
+console.log("Bem-vindo," + nome + "!");
+
+//Função para calcular o quadrado de um número
+function quadrado(numero) {
+     return numero * numero;
+}
+
+// Chama a função e exibe o resultado
+ let resultado = quadrado(4);
+ console.log("O quadrado de 4 é: " + resultado);
+
+
+ //interação do usuário no navegador
+  document.body.innerHTML = "<h1>Olá, " + nome+ "!</h1>";
